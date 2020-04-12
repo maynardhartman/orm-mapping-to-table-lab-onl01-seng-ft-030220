c91f0fe67fa053db69b308e49bc6ddbcd27ab2e4
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-12 01:01:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   end
   
   def self.create(hash)
-    student = Student.new()
+    student = Student.new(hash)
     binding.pry
     sql = <<-SQL
       INSERT INTO 'students'(name, grade) VALUES (?, ?);
