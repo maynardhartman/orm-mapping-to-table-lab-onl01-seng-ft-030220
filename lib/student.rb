f0caac80b965106913429abd0871715e17e65bc5
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-12 01:06:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,12 +38,12 @@ class Student
   end
   
   def self.create(hash)
-    student = Student.new(:name, :grade)
+    student = Student.new(@name. @grade)
     binding.pry
     sql = <<-SQL
       INSERT INTO 'students'(name, grade) VALUES (?, ?);
       SQL
-    DB[:conn].execute(sql, hash[name], hash[grade])
+    DB[:conn].execute(sql, slef.name, self.grade)
     student.save
     student 
   end
