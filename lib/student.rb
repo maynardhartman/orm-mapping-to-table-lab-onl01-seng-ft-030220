1cd4008d4b6c8c24463c015d86adfc463af12f29
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-12 00:56:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
     sql = <<-SQL
       INSERT INTO 'students'(name, grade) VALUES (?, ?);
       SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, hash.name, hash.grade)
     student.save
     student 
   end
